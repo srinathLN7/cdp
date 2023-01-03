271fe41fcb837d3cf6044549d295b3838c66bbf5
srinathLN7 <srinath0393@gmail.com>
2023-01-03 11:59:43 +0100

    reform onchain validator

--- src/FiftyFifty.hs
+++ src/FiftyFifty.hs
@@ -30,21 +30,19 @@ import           Prelude              (Semigroup (..), Show (..), String)
 import qualified Prelude
 
 data Game = Game
-    { gFirst           :: !PaymentPubKeyHash
-    , gSecond          :: !PaymentPubKeyHash
+    { gChallenger      :: !PaymentPubKeyHash
+    , gGuesser         :: !PaymentPubKeyHash
     , gStake           :: !Integer
-    , gGuessDeadline1   :: !POSIXTime
-    , gGuessDeadline2   :: !POSIXTime
-    , gProveDeadline1  :: !POSIXTime
-    , gProveDeadline2  :: !POSIXTime
+    , gGuessDeadline   :: !POSIXTime
+    , gProveDeadline   :: !POSIXTime
     , gNFT             :: !AssetClass
     } deriving (Show, Generic, FromJSON, ToJSON, Prelude.Eq, Prelude.Ord)
 
 PlutusTx.makeLift ''Game
 
 data GameChoice = A | B | C | D
-    deriving (Show, Generic, FromJSON, ToJSON, ToSchema, Prelude.Eq, Prelude.Ord)
-
+    deriving (Show, Generic, FromJSON, ToJSON, ToSchema, Prelude.Eq)
+ 
 instance Eq GameChoice where
     {-# INLINABLE (==) #-}
     A == A = True
@@ -64,7 +62,7 @@ instance Eq GameDatum where
 
 PlutusTx.unstableMakeIsData ''GameDatum
 
-data GameRedeemer =   Guess GameChoice 
+data GameRedeemer =   Guess [GameChoice, GameChoice] 
                     | Prove BuiltinByteString GameChoice
                     | ClaimChallenger 
                     | ClaimGuesser
@@ -82,94 +80,113 @@ gameDatum md = do
     Datum d <- md
     PlutusTx.fromBuiltinData d
 
+
+bsA, bsB, bsC, bsD :: BuiltinByteString
+bsA = "A"
+bsB = "B"
+bsC = "C"
+bsD = "D"
+
+unsafeFromGameChoice :: GameChoice -> BuiltinByteString
+unsafeFromGameChoice A = bsA
+unsafeFromGameChoice B = bsB
+unsafeFromGameChoice C = bsC
+unsafeFromGameChoice D = bsD 
+
+
 {-# INLINABLE mkGameValidator #-}
 mkGameValidator :: Game -> GameDatum -> GameRedeemer -> ScriptContext -> Bool 
 mkGameValidator game dat red ctx = 
-    traceIfFalse "token missing from input" (assetClassValueOf (txOutValue ownInput) (gNFT game) == 1) &&
+    traceIfFalse "game NFT missing from UTXO input" (assetClassValueOf (txOutValue ownInput) (gNFT game) == 1) &&
     case (dat, red) of 
-        --guesser makes first attempt
-        -- game CONTINUES
-        (GameDatum bs [Nothing, Nothing], Guess c) ->
-            traceIfFalse "not signed by guesser"        (txSignedBy info (unPaymentPubKeyHash $ gSecond game))                      && 
+        --guesser makes two choices c1 and c2
+        --game CONTINUES
+        (GameDatum bs [Nothing, Nothing], Guess [c1, c2]) ->
+            traceIfFalse "not signed by guesser"        (txSignedBy info (unPaymentPubKeyHash $ gGuesser game))                     && 
             traceIfFalse "challenger's stake missing"   (lovelaces (txOutValue ownInput) == gStake game)                            &&
             traceIfFalse "guesser's stake missing"      (lovelaces (txOutValue ownOutput) == (2 * gStake game))                     &&
-            traceIfFalse "invalid output datum"         (outputDatum == GameDatum bs [Just c, Nothing])                             &&
-            traceIfFalse "missed first guess deadline"   (to (gGuessDeadline1 game) `contains` txInfoValidRange info)               &&
+            traceIfFalse "invalid output datum"         (outputDatum == GameDatum bs [Just c1, Just c2])                            &&
+            traceIfFalse "missed guess deadline"        (to (gGuessDeadline game) `contains` txInfoValidRange info)                 &&
             traceIfFalse "game NFT is missing"          (assetClassValueOf (txOutValue ownOutput) (gNFT game) == 1)
 
-        -- guesser first attempt (choice c') is wrong
-        -- challenger proving it that c' was not chosen
-        -- game CONTINUES
-        (GameDatum bs [Just c', Nothing], Prove nonce c) ->
-            traceIfFalse "not signed by challenger"      (txSignedBy info (unPaymentPubKeyHash $ gFirst game))                      &&                
-            traceIfFalse "invalid proof"                 (isvalidProof bs nonce c)                                                  &&
-            traceIfFalse "missed first prove deadline"   (to (gProveDeadline1 game) `contains` txInfoValidRange info)               &&
-            traceIfFalse "invalid output datum"          (outputDatum == GameDatum bs [Just c', Nothing])                           &&
+        --guesser's choices c1 and c2 were both incorrect
+        --challenger proves choice c was chosen instead of c1 and c2
+        --game OVER
+        (GuessDatum bs [Just c1, Just c2], Prove nonce c) ->
+            traceIfFalse "not signed by challenger"      (txSignedBy info (unPaymentPubKeyHash $ gChallenger game))                 &&   
+            traceIfFalse "invalid proof"                 (isvalidProof bs nonce c c1 c2)                                            &&    
+            traceIfFalse "missed prove deadline"         (to (gProveDeadline game) `contains` txInfoValidRange info)                &&
+            traceIfFalse "invalid output datum"          (outputDatum == GameDatum bs [Just c1, Just c2])                           &&
             traceIfFalse "invalid stake"                 (lovelaces (txOutValue ownInput) == (2 * gStake game))                     &&
-            traceIfFalse "game NFT is missing"           (assetClassValueOf (txOutValue ownOutput) (gNFT game) == 1)
+            traceIfFalse "NFT must go to challenger"      nftToFirst
 
-        -- guesser makes second attempt
-        -- game CONTINUES
-        (GameDatum bs [Just c', Nothing], Guess c) ->
-            traceIfFalse "not signed by guesser"        (txSignedBy info (unPaymentPubKeyHash $ gSecond game))                      && 
-            traceIfFalse "invalid stake"                (lovelaces (txOutValue ownOutput) == (2 * gStake game))                     &&
-            traceIfFalse "invalid output datum"         (outputDatum == GameDatum bs [Just c', Just c])                             &&
-            traceIfFalse "missed second guess deadline"  (to (gGuessDeadline2 game) `contains` txInfoValidRange info)               &&
-            traceIfFalse "game NFT is missing"          (assetClassValueOf (txOutValue ownOutput) (gNFT game) == 1)
-
-        -- guesser second attempt (choice c'') is still wrong
-        -- challenger proving it that c' was not chosen
-        -- game OVER
-        (GameDatum bs [Just c', Just c''], Prove nonce c) ->
-            traceIfFalse "not signed by challenger"      (txSignedBy info (unPaymentPubKeyHash $ gFirst game))                      &&
-            traceIfFalse "invalid proof"                 (isvalidProof bs nonce c)                                                  &&
-            traceIfFalse "missed second prove deadline"  (to (gProveDeadline2 game) `contains` txInfoValidRange info)               && 
-            traceIfFalse "invalid output datum"          (outputDatum == GameDatum bs [Just c', Just c''])                          &&
-            traceIfFalse "invalid stake"                 (lovelaces (txOutValue ownOutput) == (2 * gStake game))                    &&
-            traceIfFalse "NFT must go to challenger"     nftToFirst
-
-        -- guesser no longer responds
-        -- game OVER
+        --guesser no longer responds
+        --challenger gets the stake back
         (GameDatum _ [Nothing, Nothing], ClaimChallenger) ->
-            traceIfFalse "not signed by challenger"      (txSignedBy info (unPaymentPubKeyHash $ gFirst game))                      &&    
-            traceIfFalse "too early"                     (from (1 + gGuessDeadline2 game) `contains` txInfoValidRange info)         &&
-            traceIfFalse "stake missing"                 ( (lovelaces (txOutValue ownInput) == gStake game) 
-                                                           || (lovelaces (txOutValue ownInput) == 2 * gStake game) )                &&
+            traceIfFalse "not signed by challenger"      (txSignedBy info (unPaymentPubKeyHash $ gChallenger game))                 &&    
+            traceIfFalse "too early"                     (from (1 + gGuessDeadline game) `contains` txInfoValidRange info)          &&
+            traceIfFalse "stake missing"                 (lovelaces (txOutValue ownInput) == gStake game)                           &&                                                              
+            traceIfFalse "NFT must go to challenger"      nftToFirst
 
-            traceIfFalse "NFT must go to challenger"     nftToFirst
 
-        -- challenger no longer responds
+        -- challenger no longer responds. guesser WINS.   
         -- game OVER
         (GameDatum _, [_, _], ClaimGuesser) ->                                                                  
-            traceIfFalse "not signed by guesser"        (txSignedBy info (unPaymentPubKeyHash $ gSecond game))                      &&  
-            traceIfFalse "too early"                     (from (1 + gProveDeadline2 game) `contains` txInfoValidRange info)         &&
+            traceIfFalse "not signed by guesser"         (txSignedBy info (unPaymentPubKeyHash $ gGuesser game))                    &&  
+            traceIfFalse "too early"                     (from (1 + gProveDeadline game) `contains` txInfoValidRange info)          &&
             traceIfFalse "invalid stake"                 (lovelaces (txOutValue ownOutput) == (2 * gStake game))                    &&
-            traceIfFalse "NFT must go to challenger"     nftToFirst
+            traceIfFalse "NFT must go to challenger"      nftToFirst
 
         _ -> False
 
      where
-         info :: TxInfo
-         info = scriptContextTxInfo ctx
+        info :: TxInfo
+        info = scriptContextTxInfo ctx
 
-         ownInput :: TxOut
-         ownInput = case findOwnInput ctx of
+        ownInput :: TxOut
+        ownInput = case findOwnInput ctx of
             Nothing -> traceError "game input missing"
             Just i  -> txInInfoResolved i
 
-         ownOutput :: TxOut
-         ownOutput = case getContinuingOutputs ctx of
+        ownOutput :: TxOut
+        ownOutput = case getContinuingOutputs ctx of
             [o] -> o
             _   -> traceError "expected exactly one game output" 
 
 
-         outputDatum :: GameDatum
-         outputDatum = case gameDatum $ txOutDatumHash ownOutput >>= flip findDatum info of
+        outputDatum :: GameDatum
+        outputDatum = case gameDatum $ txOutDatumHash ownOutput >>= flip findDatum info of
             Nothing -> traceError "game output datum not found"
             Just d  -> d
         
-         isvalidProof :: BuiltinByteString -> BuiltinByteString -> GameChoice -> Bool
-         isValidProof bs nonce c = sha_256 (nonce `appendByteString` c) == bs
+        isvalidProof :: BuiltinByteString -> BuiltinByteString -> GameChoice -> GameChoice -> GameChoice -> Bool
+        isValidProof bs nonce c c1 c2 = sha_256 (nonce `appendByteString` $unsafeFromGameChoice c) == bs && c /= c1 && c /= c2
 
-         nftToFirst :: Bool
-         nftToFirst = assetClassValueOf (valuePaidTo info $ unPaymentPubKeyHash $ gFirst game) (gToken game) == 1 
+        nftToChallenger :: Bool
+        nftToChallenger = assetClassValueOf (valuePaidTo info $ unPaymentPubKeyHash $ gChallenger game) (gNFT game) == 1 
+
+
+data Gaming
+    instance Scripts.ValidatorTypes Gaming where
+        type instance DatumType Gaming = GameDatum
+        type instance RedeemerType Gaming = GameRedeemer
+
+    
+typedGameValidator :: Game -> Scripts.TypedValidator Gaming
+typedGameValidator game = Scripts.mkTypedValidator @Gaming
+        ($$(PlutusTx.compile [|| mkGameValidator ||])
+        `PlutusTx.applyCode` PlutusTx.liftCode game
+        `PlutusTx.applyCode` PlutusTx.liftCode bsA
+        `PlutusTx.applyCode` PlutusTx.liftCode bsB
+        `PlutusTx.applyCode` PlutusTx.liftCode bsC
+        `PlutusTx.applyCode` PlutusTx.liftCode bsD)
+    $$(PlutusTx.compile [|| wrap ||])
+  where
+    wrap = Scripts.wrapValidator @GameDatum @GameRedeemer
+
+
+gameValidator :: Game -> Validator
+gameValidator = Scripts.validatorScript . typedGameValidator
+
+gameAddress :: Game -> Ledger.Address
+gameAddress = scriptAddress . gameValidator    
